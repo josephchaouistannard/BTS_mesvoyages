bbdc0eb23cf88c566beecef3d1b39801f56ffae6
Joseph Chaoui-Stannard <116297959+josephchaouistannard@users.noreply.github.com>
2025-10-29 13:18:55 +0100

    Phase 20 : sécurité

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -33,32 +33,35 @@ class VoyagesController extends AbstractController {
     public function index(): Response {
         $visites = $this->repository->findAllOrderBy('datecreation', 'DESC');
         return $this->render("pages/voyages.html.twig", [
-            'visites' => $visites
+                    'visites' => $visites
         ]);
     }
-    
+
     #[Route('/voyages/tri/{champ}/{ordre}', name: 'voyages.sort')]
     public function sort($champ, $ordre): Response {
         $visites = $this->repository->findAllOrderBy($champ, $ordre);
         return $this->render("pages/voyages.html.twig", [
-            'visites' => $visites
+                    'visites' => $visites
         ]);
     }
-    
-    #[Route('/voyages/recherche/{champ}', name:'voyages.findallequal')]
+
+    #[Route('/voyages/recherche/{champ}', name: 'voyages.findallequal')]
     public function findAllEqual($champ, Request $request): Response {
-        $valeur = $request->get('recherche');
-        $visites = $this->repository->findByEqualValue($champ, $valeur);
-        return $this->render("pages/voyages.html.twig", [
-            'visites' => $visites
-        ]);
+        if ($this->isCsrfTokenValid('filtre_' . $champ, $request->get('_token'))) {
+            $valeur = $request->get('recherche');
+            $visites = $this->repository->findByEqualValue($champ, $valeur);
+            return $this->render("pages/voyages.html.twig", [
+                        'visites' => $visites
+            ]);
+        }
+        return $this->redirectToRoute("voyages");
     }
-    
-    #[Route('/voyages/voyage/{id}', name:'voyages.showone')]
+
+    #[Route('/voyages/voyage/{id}', name: 'voyages.showone')]
     public function showOne($id): Response {
         $visite = $this->repository->find($id);
         return $this->render("pages/voyage.html.twig", [
-            'visite' => $visite
+                    'visite' => $visite
         ]);
     }
 }
