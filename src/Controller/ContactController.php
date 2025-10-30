ae4fedb6c59fb8e67d89ed9e0ddecde838ab338a
Joseph Chaoui-Stannard <116297959+josephchaouistannard@users.noreply.github.com>
2025-10-30 12:05:21 +0100

    Phase 24 : déploiement

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -39,7 +39,7 @@ class ContactController extends AbstractController {
     public function sendEmail(MailerInterface $mailer, Contact $contact) {
         $email = (new Email())
                 ->from($contact->getEmail())
-                ->to('contact@mesvoyages.com')
+                ->to('josephchaouistannard@gmail.com')
                 ->subject('Message du site de voyages')
                 ->html($this->renderView(
                         'pages/_email.html.twig', [
